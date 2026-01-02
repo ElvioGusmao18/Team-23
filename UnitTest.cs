bf92b18b9c05c6ea658dbefe134fc495503f06b7
ElvioGusmao18 <Gusmao-E@ulster.ac.uk>
2026-01-02 17:30:35 +0000

    Add initial unit test class and method
    Initial setup for unit testing in QuizSystem.

---
+++ UnitTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace QuizSystemTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // add unit tests here
+        }
+    }
+}
