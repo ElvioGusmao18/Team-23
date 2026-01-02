ac5a97fcfdc74628b1b2d1f707ec7d28b616d3ea
Eoin Morgan <Morgan-E18@ulster.ac.uk>
2026-01-02 17:52:32 +0000

    Update AssemblyInformationalVersion attribute

--- QuizProjectCOM326/obj/Debug/net8.0/QuizProjectCOM326.AssemblyInfo.cs
+++ QuizProjectCOM326/obj/Debug/net8.0/QuizProjectCOM326.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizProjectCOM326")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eed31c3257c2390303f1b0bde6b173dfdd130ea0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69b4a6b6fd38c5de21a0faa0df11df326304ef14")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizProjectCOM326")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizProjectCOM326")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
