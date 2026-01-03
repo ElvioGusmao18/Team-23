98b34bf750a930d14be6b17cc17523906af61918
jason <jason@CHRISSIES_PC>
2026-01-03 12:38:05 +0000

    Quiz has been implemented

---
+++ Quiz/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("=== Quiz System ===");
+Console.WriteLine("1. Start Quiz");
+Console.WriteLine("2. Exit");
+
---
+++ QuizProjectCOM326/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuizProjectCOM326
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("QuizProjectCOM326");
+
+            // Minimal run test (safe for coursework)
+            var name = new LinkedList<string>(new[] { "Programming" });
+            var desc = new LinkedList<string>(new[] { "Concepts of object-oriented programming" });
+
+            var category = new Category(1, name, desc);
+            var quiz = new Quiz("OOP Fundamentals", "Covers basics of OOP", category);
+
+            Console.WriteLine(quiz.ToString());
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+} 
